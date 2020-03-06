4840906b2efb22680177d69e793756ffedea7f79
MinersWin <minerswin@minerswin.de>
2020-03-06 09:23:37 +0100

    Added Automatic Detection + Detection Button

--- GUI.ps1
+++ GUI.ps1
@@ -1,3 +1,18 @@
 Add-Type -AssemblyName System.Windows.Forms
 . (Join-Path $PSScriptRoot 'GUI.designer.ps1')
+.\Detect_Scale.ps1
+$ButtonDetect.Add_Click{(.\Detect_Scale.ps1)}
+<#
+100 {reg import '.\Windows 10 + 8 TGFMOUSEFIX 100%.reg'}
+  125 {reg import '.\Windows 10 + 8 TGFMOUSEFIX 125%.reg'}
+  150 {reg import '.\Windows 10 + 8 TGFMOUSEFIX 150%.reg'}
+  175 {reg import '.\Windows 10 + 8 TGFMOUSEFIX 175%.reg'}
+  200 {reg import '.\Windows 10 + 8 TGFMOUSEFIX 200%.reg'}
+  225 {reg import '.\Windows 10 + 8 TGFMOUSEFIX 225%.reg'}
+  250 {reg import '.\Windows 10 + 8 TGFMOUSEFIX 250%.reg'}
+  300 {reg import '.\Windows 10 + 8 TGFMOUSEFIX 300%.reg'}
+  350 {reg import '.\Windows 10 + 8 TGFMOUSEFIX 350%.reg'}
+  default {reg import '.\Windows 10 + 8 Standard.reg'}
+#>
+
 $FormMousePack.ShowDialog()
