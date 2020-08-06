953522c1cb71f5a0e44bdac4994de2753f7bad79
MinersWin <minerswin@minerswin.de>
2020-08-06 03:55:05 +0200

    Added System Drawing Dynamic-Link Libary + Reference

--- GUI.ps1
+++ GUI.ps1
@@ -4,6 +4,28 @@
 #04.08.2020
 Add-Type -AssemblyName System.Windows.Forms
 . (Join-Path $PSScriptRoot 'GUI.designer.ps1')
+
+Add-Type @'
+  using System; 
+  using System.Runtime.InteropServices;
+  using System.Drawing;
+  public class DPI {  
+    [DllImport("gdi32.dll")]
+    static extern int GetDeviceCaps(IntPtr hdc, int nIndex);
+    public enum DeviceCap {
+      VERTRES = 10,
+      DESKTOPVERTRES = 117
+    } 
+    public static float scaling() {
+      Graphics g = Graphics.FromHwnd(IntPtr.Zero);
+      IntPtr desktop = g.GetHdc();
+      int LogicalScreenHeight = GetDeviceCaps(desktop, (int)DeviceCap.VERTRES);
+      int PhysicalScreenHeight = GetDeviceCaps(desktop, (int)DeviceCap.DESKTOPVERTRES);
+      return (float)PhysicalScreenHeight / (float)LogicalScreenHeight;
+    }
+  }
+'@ -ReferencedAssemblies 'System.Drawing.dll'
+
 .\Detect_Scale.ps1
 
 ###################################################################################
