5bd469c8df04bbfe43b0c72b037725a37b115cf4
MinersWin <minerswin@minerswin.de>
2020-08-09 23:44:35 +0200

    Added Reset Script

---
+++ MouseFix/Reset.ps1
@@ -0,0 +1,7 @@
+#Created by MinersWin
+#TGF Mouse Tuning Pack 2.0
+#https://tuning-pack.de
+#09.08.2020
+
+Set-ItemProperty -Path "HKEY_CURRENT_USER\Control Panel\Mouse" -Name "SmoothMouseXCurve" -Value 00,00,00,00,00,00,00,00,15,6e,00,00,00,00,00,00,00,40,01,00,00,00,00,00,29,dc,03,00,00,00,00,00,00,00,28,00,00,00,00,00 -ErrorAction SilentlyContinue
+Set-ItemProperty -Path "HKEY_CURRENT_USER\Control Panel\Mouse" -Name "SmoothMouseYCurve" -Value 00,00,00,00,00,00,00,00,fd,11,01,00,00,00,00,00,00,24,04,00,00,00,00,00,00,fc,12,00,00,00,00,00,00,c0,bb,01,00,00,00,00 -ErrorAction SilentlyContinue
