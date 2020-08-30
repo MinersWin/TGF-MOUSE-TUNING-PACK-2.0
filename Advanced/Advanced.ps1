f7843f934e5d80444936a3261d642a3528f5acb9
MinersWin <minerswin@minerswin.de>
2020-08-30 17:08:11 +0200

    Added Varialbe Insert to Advanced Form

--- Advanced/Advanced.ps1
+++ Advanced/Advanced.ps1
@@ -1,5 +1,7 @@
 Add-Type -AssemblyName System.Windows.Forms
 . (Join-Path $PSScriptRoot 'Advanced.designer.ps1')
+
+#Get Variables
 $VariableAdvancedBeep = Get-ItemProperty -Path "HKCU:\Control Panel\Mouse" -Name Beep
 $VariableAdvancedDoubleClickHeight = Get-ItemProperty -Path "HKCU:\Control Panel\Mouse" -Name DoubleClickHeight
 $VariableAdvancedDoubleClickSpeed = Get-ItemProperty -Path "HKCU:\Control Panel\Mouse" -Name DoubleClickSpeed
@@ -17,4 +19,31 @@ $VariableAdvancedSmoothMouseXCurve = Get-ItemProperty -Path "HKCU:\Control Panel
 $VariableAdvancedSmoothMouseYCurve = Get-ItemProperty -Path "HKCU:\Control Panel\Mouse" -Name SmoothMouseYCurve
 $VariableAdvancedSnapToDefaultButton = Get-ItemProperty -Path "HKCU:\Control Panel\Mouse" -Name SnapToDefaultButton
 $VariableAdvancedSwapMouseButtons = Get-ItemProperty -Path "HKCU:\Control Panel\Mouse" -Name SwapMouseButtons
+
+#Set Variables to Form
+    $ComboBoxAdvancedBeep.Items.Add("Yes")
+    $ComboBoxAdvancedBeep.Items.Add("No")
+    if ($VariableAdvancedBeep.Beep -eq "Yes"){
+        $ComboBoxAdvancedBeep.SelectedItem = "Yes"
+    } else {
+        $ComboBoxAdvancedBeep.SelectedItem = "No"
+    }
+$TextBoxAdvancedDoubleClickHeight.Text = $VariableAdvancedDoubleClickHeight.DoubleClickHeight
+$TextBoxAdvancedDoubleClickSpeed.Text = $VariableAdvancedDoubleClickSpeed.DoubleClickSpeed
+$TextBoxAdvancedDoubleClickWidth.Text = $VariableAdvancedDoubleClickWidth.DoubleClickWidth
+$TextBoxAdvancedExtendedSounds.Text = $VariableAdvancedExtendedSounds.ExtendedSounds
+$TextBoxAdvancedMouseHoverHeight.Text = $VariableAdvancedMouseHoverHeight.MouseHoverHeight
+$TextBoxAdvancedMouseHoverTime.Text = $VariableAdvancedMouseHoverTime.MouseHoverTime
+$TextBoxAdvancedMouseHoverWidth.Text = $VariableAdvancedMouseHoverWidth.MouseHoverWidth
+$TextBoxAdvancedMouseSensitivity.Text = $VariableAdvancedMouseSensitivity.MouseSensitivity
+$TextBoxAdvancedMouseSpeed.Text = $VariableAdvancedMouseSpeed.MouseSpeed
+$TextBoxAdvancedMouseThreshold1.Text = $VariableAdvancedMouseThreshold1.MouseThreshold1
+$TextBoxAdvancedMouseThreshold2.Text = $VariableAdvancedMouseThreshold2.MouseThreshold2
+$TextBoxAdvancedMouseTrails.Text = $VariableAdvancedMouseTrails.MouseTrails
+$TextBoxAdvancedSmoothMouseXCurve.Text = $VariableAdvancedSmoothMouseXCurve.SmoothMouseXCurve
+$TextBoxAdvancedMouseYCurve.Text = $VariableAdvancedSmoothMouseYCurve.SmoothMouseYCurve
+$TextBoxAdvancedSnapToDefaultButton.Text = $VariableAdvancedSnapToDefaultButton.SnapToDefaultButton
+$TextBoxAdvancedSwapMouseButtons.Text = $VariableAdvancedSwapMouseButtons.SwapMouseButtons
+
+
 $FormAdvanced.ShowDialog()
